319492436fcc422a2fe4b563ef3705289a1bd294
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-04-07 09:25:17 -0400

    2nd attempt of reverseString

--- reverseString.js
+++ reverseString.js
@@ -3,6 +3,7 @@ Source LeetCode
 344 Reverse String
 (https://leetcode.com/problems/reverse-string/)
 1st 2022-04-06
+2nd 2022-04-07
 
 Write a function that reverses a string. The input string is given as 
 an array of characters s.
@@ -15,7 +16,7 @@ memory.
 
 // 1st Attempt
 // LOGIC: Swap the first and the last
-// Time: O(n)  |  Memory: n+1
+// Time: O(n)  |  Memory: O(1)
 var reverseString = function(s) {
     let len = s.length
     for(let i = 0; i < (len/2); i++){  
@@ -24,6 +25,13 @@ var reverseString = function(s) {
         s[len-i-1] = temp;
     }
     return s;
+};
+
+// 2nd Attempt
+// LOGIC: Reverse using reverse()
+// Time: O(n)  |  Memory: O(1)
+var reverseString = function(s) {
+    return s.reverse();
 };
 
 // TEST
