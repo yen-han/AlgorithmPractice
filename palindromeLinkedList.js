7cf26fda08ff989d28f340b7ce0013ed12e034d2
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-04-18 19:42:52 -0400

    2nd attempt of palindromeLinkedList

--- palindromeLinkedList.js
+++ palindromeLinkedList.js
@@ -30,6 +30,34 @@ var isPalindrome = function(head) {
     return fromStart === fromEnd;
 };
 
+// 2nd Attempt
+// LOGIC: Find the size of the linked list & make stack in half way &
+// pop element if the same value occurs in the order
+// Time: O(n)  |  Memory: O(1)  
+var isPalindrome = function(head) {
+    let current = head;
+    let stack = [];
+    let size = 0;
+    while(current){
+        current = current.next;
+        size++;
+    }
+    if(size === 0 || size === 1 ) return true;
+    current = head;
+    for(let i = 0;i < size/2; i++){
+        stack.push(current.val);
+        current = current.next;
+    }
+    if(size % 2 === 1) stack.pop();
+    while(current){
+        if(stack[stack.length-1] === current.val){
+            stack.pop();
+        }
+        current = current.next;
+    }
+    return stack.length === 0;
+};
+
 // TEST
 // head = [1,2,2,1]
 // Output: true
