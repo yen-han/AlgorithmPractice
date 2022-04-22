f387c5ed08fcb3dae5ec29c6eb310635c2248434
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-04-21 20:30:49 -0400

    2nd attempt of implementQueueUsingStacks

--- implementQueueUsingStacks.js
+++ implementQueueUsingStacks.js
@@ -50,6 +50,49 @@ MyQueue.prototype.empty = function() {
     return this.stack1.length === 0;
 };
 
+// 2nd Attempt
+// LOGIC : Going back and forth of 2 stacks
+// Using only standard operations of stack
+var MyQueue = function() {
+    this.stack1 = [];
+    this.stack2 = [];
+};
+
+// @param {number} x
+// @return {void}
+MyQueue.prototype.push = function(x) {
+    this.stack1.push(x);
+};
+
+// @return {number}
+MyQueue.prototype.pop = function() {
+    while(this.stack1.length){
+        this.stack2.push(this.stack1.pop());
+    }
+    let pop = this.stack2.pop();
+    while(this.stack2.length){
+        this.stack1.push(this.stack2.pop());
+    }
+    return pop;
+};
+
+// @return {number}
+MyQueue.prototype.peek = function() {
+    while(this.stack1.length){
+        this.stack2.push(this.stack1.pop());
+    }
+    let peek = this.stack2[this.stack2.length-1];
+    while(this.stack2.length){
+        this.stack1.push(this.stack2.pop());
+    }
+    return peek;
+};
+
+// @return {boolean}
+MyQueue.prototype.empty = function() {
+    return this.stack1.length === 0 && this.stack2.length === 0;
+};
+
 /** 
  * Your MyQueue object will be instantiated and called as such:
  * var obj = new MyQueue()
