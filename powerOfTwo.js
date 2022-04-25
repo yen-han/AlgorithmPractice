286339cb4add48f577fc8e757e2362f2f73d63af
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-04-25 14:29:58 -0400

    1st attempt of powerOfTwo

---
+++ powerOfTwo.js
@@ -0,0 +1,28 @@
+/*********************************************************************** 
+Source LeetCode
+231 Power of Two
+(https://leetcode.com/problems/power-of-two/)
+1st 2022-04-25
+
+Given an integer n, return true if it is a power of two. Otherwise, 
+return false.
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+ * @param {number} n
+ * @return {boolean}
+************************************************************************/
+
+// 1st Attempt
+// LOGIC: Recursion 
+var isPowerOfTwo = function(n) {
+    if(n <= 0){
+        return false;
+    } else if(n === 1){
+        return true;
+    } else if(n % 2 !== 0){
+        return false;
+    }
+    else {
+        return isPowerOfTwo(n/2);
+    }
+};
