320850daed1b32d63b668296a1942bae6d98a349
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-05-06 21:21:33 -0400

    1st attempt of lowestCommonAncestorOfBinarySearchTree

---
+++ lowestCommonAncestorOfBinarySearchTree.js
@@ -0,0 +1,36 @@
+/*********************************************************************** 
+Source LeetCode
+141 Linked List Cycle
+(https://leetcode.com/problems/linked-list-cycle/)
+1st 2022-05-05
+
+Given a binary search tree (BST), find the lowest common ancestor (LCA) 
+of two given nodes in the BST.
+
+According to the definition of LCA on Wikipedia: “The lowest common 
+ancestor is defined between two nodes p and q as the lowest node in T 
+that has both p and q as descendants (where we allow a node to be a 
+descendant of itself).”
+
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+************************************************************************/
+
+// 1st Attempt
+// LOGIC: Check for both p and q are lower or higher than root.
+// Return if p, q are between root value or either p, q is same as root value
+// Time : O(n)
+var lowestCommonAncestor = function(root, p, q) {
+    if(!root) return null;
+    if(p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q); 
+    else if(p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q); 
+    return root;
+};
