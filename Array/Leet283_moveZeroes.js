f2b1af1130442411d1c87291a2632f03a3b70245
Yeaeun Han <91946024+YeaaeunHan@users.noreply.github.com>
2022-08-02 21:32:17 -0400

    1st attempt of moveZeroes

---
+++ Array/Leet283_moveZeroes.js
@@ -0,0 +1,43 @@
+/*********************************************************************** 
+Source LeetCode
+283 Move Zeroes
+(https://leetcode.com/problems/move-zeroes/)
+1st 2022-08-02
+
+Given an integer array nums, move all 0's to the end of it while 
+maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Constraints:
+    1 <= nums.length <= 104
+    -231 <= nums[i] <= 231 - 1
+
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+************************************************************************/
+
+// 1st Attempt
+// LOGIC: Count 0, move non-zero integer to the front and fill the 0
+// Time: O(n) Space: O(1)
+var moveZeroes = function(nums) {
+    let count = 0;
+    // Count for 0 in nums
+    for(let i = 0;i < nums.length; i++){
+        if(nums[i] == 0) count++;
+    }
+    // If 0 exists in the array
+    if(count > 0){
+        // Move non-zero integer to the front
+        for(let i = 0, j = 0;i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        // Fill the 0 fromm the end
+        for(let i = nums.length-1, j = 0;j < count; i--, j++){
+            nums[i] = 0
+        }
+    }
+};
