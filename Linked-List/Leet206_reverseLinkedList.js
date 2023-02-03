6d5f7261cc4532e0de8e751442b02f7ad4671a1f
Yeaeun Han <yenhan.dev@gmail.com>
2023-02-02 23:17:31 -0500

    2nd attempt of reverseLinkedList in JavaScript

--- Linked-List/Leet206_reverseLinkedList.js
+++ Linked-List/Leet206_reverseLinkedList.js
@@ -3,6 +3,7 @@ Source LeetCode
 206 Reverse Linked List
 (https://leetcode.com/problems/reverse-linked-list/description/)
 1st 2023-01-01
+2nd 2023-02-02
 
 Given the head of a singly linked list, reverse the list, and return 
 the reversed list.
@@ -35,4 +36,17 @@ var reverseList = function (head) {
     if (forward) node = forward;
     else return node;
   }
+};
+
+// 2nd Attempt
+// LOGIC: Recursion, change direction from next node to current node
+//        & make current node's reference field point to null
+// Time : -  |  Memory: O(1)
+var reverseList = function (head) {
+  if (head == null || head.next == null) return head;
+  let next = head.next;
+  let list = reverseList(next);
+  next.next = head;
+  head.next = null;
+  return list;
 };
