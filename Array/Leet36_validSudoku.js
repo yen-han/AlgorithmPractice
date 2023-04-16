a193e08687465eb6168f86937a585a0c64b484b6
Yeaeun Han <yenhan.dev@gmail.com>
2023-04-16 10:47:48 -0400

    update logic of validSudoku

--- Array/Leet36_validSudoku.js
+++ Array/Leet36_validSudoku.js
@@ -28,7 +28,7 @@ Constraints:
 ************************************************************************/
 
 // 1st Attempt
-// LOGIC: Using temp array to store
+// LOGIC: Check if each row, column, and 3*3 sub-box follows the sudoku rules.
 // Time: O(n^2) Space: O(n^2)
 var isValidSudoku = function (board) {
   // Hash set
