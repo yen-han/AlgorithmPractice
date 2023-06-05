6be77402cebfe1eca472f936fbbc4a74c03f2ad8
Yeaeun Han <yenhan.dev@gmail.com>
2023-06-05 15:34:42 -0400

    2nd attempt of longestSubstringWithoutRepeatingCharacters

--- String/Leet3_LongestSubstringWithoutRepeatingCharacters.js
+++ String/Leet3_LongestSubstringWithoutRepeatingCharacters.js
@@ -3,6 +3,7 @@ Source LeetCode
 3. Longest Substring Without Repeating Characters
 https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 1st 2023-05-25
+1st 2023-06-05
 
 Given a string s, find the length of the longest substring without 
 repeating characters.
@@ -26,26 +27,43 @@ Constraints:
 ************************************************************************/
 
 // 1st Attempt
-// LOGIC: using sliding window, keep track of start index and
+// LOGIC: using Set, keep track of start index and
 // find longest substring without repeating characters
-// Time: O(n)  |  Memory: O(n)
+// Time: O(n^2)  |  Memory: O(n)
 var lengthOfLongestSubstring = function (s) {
-  let start = 0;
-  let longest = 0;
   let substring = new Set();
-
+  let longest = 0;
+  let start = 0;
   for (let i = 0; i < s.length; i++) {
-    if (substring.has(s[i])) {
-      if (substring.size > longest) {
-        longest = substring.size;
-      }
-      substring.clear();
-      i = start;
-      start += 1;
-    } else {
+    if (!substring.has(s[i])) {
       substring.add(s[i]);
+    } else {
+      if (substring.size > longest) longest = substring.size;
+      substring.clear();
+      i = start++;
     }
   }
-  if (substring.size > longest) longest = substring.size;
+  return substring.size > longest ? substring.size : longest;
+};
+
+// 2nd Attempt
+// LOGIC: using sliding window & Set, going through each character
+// When duplicate is found, move start index to the next index of duplicate
+// Time: O(n^2)  |  Memory: O(n)
+var lengthOfLongestSubstring = function (s) {
+  let substring = new Set();
+  let longest = 0;
+  let start = 0;
+  let end = 0;
+  while (end < s.length) {
+    while (substring.has(s[end])) {
+      substring.delete(s[start]);
+      start++;
+    }
+    substring.add(s[end]);
+
+    longest = Math.max(end - start + 1, longest);
+    end++;
+  }
   return longest;
 };
