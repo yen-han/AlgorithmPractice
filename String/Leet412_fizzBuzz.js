4619282e19f11b0ce199f6a5ca9ff72708c5537f
Yeaeun Han <yenhan.dev@gmail.com>
2023-06-11 18:18:33 -0400

    3rd attempt of fizzBuzz

--- String/Leet412_fizzBuzz.js
+++ String/Leet412_fizzBuzz.js
@@ -4,6 +4,7 @@ Source LeetCode
 (https://leetcode.com/problems/fizz-buzz/)
 1st 2022-03-30
 2nd 2022-11-26
+3rd 2023-06-11
 
 Given an integer n, return a string array answer (1-indexed) where:
 
@@ -18,8 +19,33 @@ answer[i] == i (as a string) if none of the above conditions are true.
  * switch-case statement with mod() is faster than if-else.
  **********************************************************************/
 
+// 3rd Attempt
+// LOGIC: Using switch-case, Go through every integer, modulus 3, 5, or 15
+// Time O(n) | Space O(n)
+var fizzBuzz = function (n) {
+  let res = [];
+  for (let i = 1; i <= n; i++) {
+    switch (true) {
+      case i % 15 == 0:
+        res.push("FizzBuzz");
+        break;
+      case i % 3 == 0:
+        res.push("Fizz");
+        break;
+      case i % 5 == 0:
+        res.push("Buzz");
+        break;
+      default:
+        res.push(`${i}`);
+        break;
+    }
+  }
+  return res;
+};
+
 // 2nd Attempt
 // LOGIC: Go through every integer, modulus 3, 5, or 15
+// Time O(n) | Space O(n)
 let fizzBuzz = function (n) {
   let answer = [];
   for (let i = 0; i < n; i++) {
@@ -38,6 +64,7 @@ let fizzBuzz = function (n) {
 
 // 1st Attempt
 // LOGIC: Go through every integer, modulus 3, 5, or 3 and 5
+// Time O(n) | Space O(n)
 // let fizzBuzz = function(n) {
 //     let answer = [];
 //     for(let i = 0; i < n; i++){
