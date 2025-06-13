aac45606b932a1366af03802a6264e67f46a7810
Yeaeun Han <yenhan.dev@gmail.com>
2025-06-12 22:17:35 -0400

    2nd of Leet2406_divide_intervals_into_minimum_number_of_groups

--- Heap/Leet2406_divide_intervals_into_minimum_number_of_groups.py
+++ Heap/Leet2406_divide_intervals_into_minimum_number_of_groups.py
@@ -3,6 +3,7 @@ Source LeetCode
 2406. Divide Intervals Into Minimum Number of Groups
 https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/description/
 1st 2025-06-12
+2nd 2025-06-12
 
 You are given a 2D integer array intervals where intervals[i] = [lefti, righti] represents the inclusive interval [lefti, righti].
 
@@ -27,7 +28,33 @@ Constraints:
 """
 
 # 1st Attempt
-# LOGIC: Using min heap, keep track right element of intervals in groups and check overapping.
+# LOGIC: Using min heap, keep track right element of intervals in groups and check overlapping. - Time Limit Exceeded
+# Time: O(n^2)  | Space: O(n)
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        groups = []
+        heap = []
+
+        for interval in intervals:
+            heapq.heappush(heap, (interval))
+
+        while heap:
+            interval = heapq.heappop(heap)
+            if len(groups) == 0:
+                groups.append(interval[1])
+            else: 
+                flag = False
+                for i in range(len(groups)):
+                    if groups[i] < interval[0] :
+                        groups[i] = interval[1]
+                        flag = True
+                        break
+                if flag is False:
+                    groups.append(interval[1])    
+        return len(groups)
+
+# 2nd Attempt
+# LOGIC: Using min heap, keep track right element of intervals in groups and check overlapping.
 # Time: O(nlogn)  | Space: O(n)
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
