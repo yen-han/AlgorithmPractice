915f7504bd245f56c7fcb7b481af9ff09daa3e4f
Yeaeun Han <yenhan.dev@gmail.com>
2025-07-31 21:54:44 -0400

    leet146_lru_cache

--- Array/Leet146_LRU_cache.py
+++ Array/Leet146_LRU_cache.py
@@ -1,7 +1,7 @@
 """
 Source LeetCode
 146. LRU Cache
-https://leetcode.com/problems/longest-consecutive-sequence/description/
+https://leetcode.com/problems/lru-cache/
 1st: 2024-12-02
 
 Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
@@ -13,18 +13,21 @@ Implement the LRUCache class:
     void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
 
 The functions get and put must each run in O(1) average time complexity.
-Example 1:
-Input: nums = [100,4,200,1,3,2]
-Output: 4
 
-Example 2:
-Input: nums = [0,3,7,2,5,8,4,6,0,1]
-Output: 9
+Example 1:
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
 
 Constraints:
-    0 <= nums.length <= 105
-    -109 <= nums[i] <= 109
+    1 <= capacity <= 3000
+    0 <= key <= 104
+    0 <= value <= 105
+    At most 2 * 105 calls will be made to get and put.
 """
+
+# 1st Attempt
+# LOGIC : using dictionary to store key-value pairs, and maintain the order of usage.
+# Time : O(n)  | Space : O(n)
 class LRUCache:
     def __init__(self, cap: int):
         self.dict = dict() 
