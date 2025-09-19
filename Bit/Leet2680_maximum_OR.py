d8f6bff5e6c064e3ad8b87fa20880ecc6c843d90
Yeaeun Han <yenhan.dev@gmail.com>
2025-09-18 22:07:48 -0400

    leet2680 maximum OR

---
+++ Bit/Leet2680_maximum_OR.py
@@ -0,0 +1,45 @@
+"""
+Source LeetCode
+2680. Maximum OR
+https://leetcode.com/problems/maximum-or/description/
+1st 2025-9-18
+
+You are given a 0-indexed integer array nums of length n and an integer k. 
+In an operation, you can choose an element and multiply it by 2.
+
+Return the maximum possible value of nums[0] | nums[1] | ... | nums[n - 1] 
+that can be obtained after applying the operation on nums at most k times.
+
+Note that a | b denotes the bitwise or between two integers a and b.
+
+Example 1:
+Input: nums = [12,9], k = 1
+Output: 30
+
+Example 2:
+Input: nums = [8,1,2], k = 2
+Output: 35
+
+Constraints:
+    1 <= nums.length <= 105
+    1 <= nums[i] <= 109
+    1 <= k <= 15
+"""
+
+# 1st Attempt
+# LOGIC: Brute Force
+# Time: O(2^n)  | Space: O(n)
+class Solution:
+    def maximumOr(self, nums: List[int], k: int) -> int:
+        max_value = 0
+
+        for j in range(len(nums)):
+            res=0
+            for i in range(j):
+                res |= nums[i]
+            res |= (nums[j] << k)
+            for i in range(j+1,len(nums)):
+                res |= nums[i]
+            max_value = max(res, max_value)
+
+        return max_value
